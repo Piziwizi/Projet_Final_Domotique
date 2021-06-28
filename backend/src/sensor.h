32ec92bfb98d846d1ad6b9753ad236da0e8e0b71
Toma Gosselin-St-Pierre <tomastpierre@hotmail.com>
2021-06-28 17:20:00 +0100

    add state machine to sensor

--- backend/src/sensor.h
+++ backend/src/sensor.h
@@ -6,5 +6,7 @@
 void *Sensor_task(void *vargp);
 void *SensorManager_task(void *vargp);
 json_object* get_json_from_sensor(sensor_t sensor);
+void add_sensor(uint32_t id);
+void remove_sensor(uint32_t id);
 
 #endif
