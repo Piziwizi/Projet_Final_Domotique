8d2e748aa5ae8cbfdeb96155006897bc5d6300d8
Toma <toma.gosselin-st-pierre.1@ens.etsmtl.ca>
2021-08-01 14:57:29 -0400

    code cleanup

--- sensor/PlatformIO/sensor/src/define.h
+++ sensor/PlatformIO/sensor/src/define.h
@@ -2,67 +2,60 @@
 #define DEFINE_H
 #include "freertos.h"
 
-#define ENABLE_HEAT_SENSOR 1
-#define ENABLE_LIGHT_SENSOR 1
-#define ENABLE_HEAT_CONTROL 1
-#define ENABLE_LIGHT_CONTROL 1
 #define DEVICE_ID 0 // max 32
+#define RELAY_OFF 1
+#define RELAY_ON 0
 
 #define MAXRCVLEN 1000
 #define MAX_MESSAGE_LENGHT 128
 #define STRING_DELIMITER ":"
+
+#define DHTPIN 4
+#define RELAY_HEAT 23
+#define RELAY_LIGHT 22
+#define RELAY_FAN 21
+#define BUTTON_LIGHT 19
+#define BUTTON_UP 18
+#define BUTTON_DOWN 5
+
+#define DHTTYPE DHT11 // DHT 11
 
 typedef enum
 {
     UNKNOWN_MAIN,
     SENSOR,
     CONTROL,
-    REFRESH,
     BUTTON,
     SYSTEM,
+    WIFI_MAIN_LENGHT
 } wifi_main_t;
 static const char *WIFI_MAIN_TYPE_STRING[] = {"UNKNOWN",
                                               "SENSOR",
                                               "CONTROL",
-                                              "REFRESH",
                                               "BUTTON",
                                               "SYSTEM"};
-#define WIFI_MAIN_LENGHT 6
 
 typedef enum
 {
     UNKNOWN_OPERATOR,
     SET,
     GET,
+    WIFI_OPERATOR_LENGHT
 } wifi_operator_t;
 static const char *WIFI_OPERATOR_TYPE_STRING[] = {"UNKNOWN",
                                                   "SET",
                                                   "GET"};
-#define WIFI_OPERATOR_LENGHT 3
 
 typedef enum
 {
     UNKNOWN_TYPE,
     TEMP,
     LIGHT,
+    WIFI_TYPE_LENGHT
 } wifi_type_t;
 static const char *WIFI_TYPE_TYPE_STRING[] = {"UNKNOWN",
                                               "TEMP",
                                               "LIGHT"};
-#define WIFI_TYPE_LENGHT 3
-
-typedef enum
-{
-    UNKNOWN_AUTH,
-    PASSWORD,
-    SSID,
-    APPLY
-} wifi_auth_t;
-static const char *WIFI_AUTH_TYPE_STRING[] = {"UNKNOWN",
-                                              "PASSWORD",
-                                              "SSID",
-                                              "APPLY"};
-#define WIFI_AUTH_LENGHT 4
 
 typedef enum
 {
@@ -71,23 +64,15 @@ typedef enum
     START,
     RESTART,
     STOP,
-    WIFI,
     STATUS_DEVICE,
-    GET_ID
+    GET_ID,
+    WIFI_SYSTEM_LENGHT
 } wifi_system_t;
 static const char *WIFI_SYSTEM_TYPE_STRING[] = {"UNKNOWN",
                                                 "RESET",
                                                 "START",
                                                 "RESTART",
                                                 "STOP",
-                                                "WIFI",
                                                 "STATUS",
                                                 "ID"};
-#define WIFI_SYSTEM_LENGHT 8
-
-SemaphoreHandle_t mutex_read_temp, mutex_read_light, mutex_heat, mutex_light, mutex_button_heat, mutex_button_light;
-
-float read_temp, read_light = 0;
-bool heat, light = 0;
-
 #endif
--- sensor/PlatformIO/sensor/src/main.cpp
+++ sensor/PlatformIO/sensor/src/main.cpp
@@ -8,33 +8,25 @@
 #include <WiFi.h>
 #include <WiFiMulti.h>
 
-#define DHTPIN 4
-#define RELAY_HEAT 23
-#define RELAY_LIGHT 22
-#define RELAY_FAN 21
-#define BUTTON_LIGHT 19
-#define BUTTON_UP 18
-#define BUTTON_DOWN 5
-
-#define DHTTYPE DHT11 // DHT 11
-
 DHT dht(DHTPIN, DHTTYPE);
-
 WiFiClient client;
 WiFiServer server_device(3334);
-double old_temp=NAN;
-uint32_t fail=0;
-uint32_t light_switch=0;
-int32_t temp_switch=0;
-uint32_t previous_light=0;
-uint32_t previous_temp1=0;
-uint32_t previous_temp2=0;
 
+SemaphoreHandle_t mutex_read_temp, mutex_read_light;
+float read_temp, read_light = 0;
 
-//************  déclaration fonction   ************
-#if ENABLE_HEAT_SENSOR
+SemaphoreHandle_t mutex_heat, mutex_light;
+bool heat, light = 0;
+
+SemaphoreHandle_t mutex_button_heat, mutex_button_light;
+uint32_t light_switch = 0;
+int32_t temp_switch = 0;
+
+double old_temp = NAN;
+uint32_t fail = 0;
+
+//************  function declaration  ************
 float read_temperature(float *Temperature);
-#endif
 void wifi_client(void *parameter);
 void init_wifi(void);
 void apply_gpio(void *parameter);
@@ -44,60 +36,36 @@ uint32_t decode_message(String msg, WiFiClient *local);
 
 void setup()
 {
-#if ENABLE_HEAT_SENSOR
-    mutex_read_temp = xSemaphoreCreateMutex();
-    if (mutex_read_temp == NULL)
-    {
-        ESP.restart();
-    }
-#endif
-#if ENABLE_LIGHT_SENSOR
-    mutex_read_light = xSemaphoreCreateMutex();
-    if (mutex_read_light == NULL)
-    {
-        ESP.restart();
-    }
-#endif
 
-#if ENABLE_HEAT_CONTROL
-    mutex_heat = xSemaphoreCreateMutex();
-    if (mutex_heat == NULL)
-    {
-        ESP.restart();
-    }
-    pinMode(RELAY_HEAT, OUTPUT);
-    pinMode(RELAY_FAN, OUTPUT);
-#endif
-#if ENABLE_LIGHT_CONTROL
-    mutex_light = xSemaphoreCreateMutex();
-    if (mutex_light == NULL)
-    {
-        ESP.restart();
-    }
-    pinMode(RELAY_LIGHT, OUTPUT);
-#endif
-
-    mutex_button_heat = xSemaphoreCreateMutex();
-    if (mutex_button_heat == NULL)
-    {
-        ESP.restart();
-    }
-    
-    mutex_button_light = xSemaphoreCreateMutex();
-    if (mutex_button_light == NULL)
-    {
-        ESP.restart();
-    }
-
+    Serial.begin(115200);
+    dht.begin();
     pinMode(BUTTON_LIGHT, INPUT);
     pinMode(BUTTON_UP, INPUT);
     pinMode(BUTTON_DOWN, INPUT);
 
-    Serial.begin(115200);
-    dht.begin();
-    digitalWrite(RELAY_HEAT, 1);
-    digitalWrite(RELAY_FAN, 1);
-    digitalWrite(RELAY_LIGHT, 1);
+    pinMode(RELAY_HEAT, OUTPUT);
+    pinMode(RELAY_FAN, OUTPUT);
+    pinMode(RELAY_LIGHT, OUTPUT);
+
+    mutex_read_temp = xSemaphoreCreateMutex();
+    mutex_read_light = xSemaphoreCreateMutex();
+    mutex_heat = xSemaphoreCreateMutex();
+    mutex_light = xSemaphoreCreateMutex();
+    mutex_button_heat = xSemaphoreCreateMutex();
+    mutex_button_light = xSemaphoreCreateMutex();
+
+    if (mutex_read_temp == NULL ||
+        mutex_read_light == NULL ||
+        mutex_heat == NULL ||
+        mutex_light == NULL ||
+        mutex_button_heat == NULL ||
+        mutex_button_light == NULL)
+    {
+        ESP.restart();
+    }
+    digitalWrite(RELAY_HEAT, RELAY_OFF);
+    digitalWrite(RELAY_FAN, RELAY_OFF);
+    digitalWrite(RELAY_LIGHT, RELAY_OFF);
 
     xTaskCreate(
         apply_gpio,   // Function that should be called
@@ -115,9 +83,7 @@ void loop()
 {
     read_temperature(&read_temp);
 }
-//*************   FONCTION   ***************//
-
-#if ENABLE_HEAT_SENSOR
+//*************   FUNCTION   ***************//
 float read_temperature(float *Temperature) //
 {
     *Temperature = dht.readTemperature(); // put value in the global variable
@@ -138,7 +104,8 @@ float read_temperature(float *Temperature) //
         Serial.println(F("°C "));
     }
 
-    if(fail == 50){ //todo check why it fails when the resistance is running
+    if (fail == 50)
+    { //todo check why it fails when the resistance is running
         fail = 0;
         xSemaphoreTake(mutex_read_temp, portMAX_DELAY);
         old_temp = temperature;
@@ -149,60 +116,59 @@ float read_temperature(float *Temperature) //
 
     return temperature;
 }
-#endif
 
 void apply_gpio(void *parameter)
 {
-    for(;;)
+    uint32_t previous_light = 0;
+    uint32_t previous_temp1 = 0;
+    uint32_t previous_temp2 = 0;
+
+    for (;;)
     {
-#if ENABLE_HEAT_CONTROL
+        //Relays ---------------------------------
         xSemaphoreTake(mutex_heat, portMAX_DELAY);
-        if(heat != 1){
-            digitalWrite(RELAY_HEAT, HIGH);
-            digitalWrite(RELAY_FAN, LOW);
-        } else {
-            digitalWrite(RELAY_HEAT, LOW);
-            digitalWrite(RELAY_FAN, HIGH);
-        }
+        digitalWrite(RELAY_HEAT, heat ? RELAY_ON : RELAY_OFF);
+        digitalWrite(RELAY_FAN, !heat ? RELAY_ON : RELAY_OFF);
         xSemaphoreGive(mutex_heat);
-#endif
-#if ENABLE_LIGHT_CONTROL
+
         xSemaphoreTake(mutex_light, portMAX_DELAY);
-        if(light != 1){
-            digitalWrite(RELAY_LIGHT, HIGH);
-        } else {
-            digitalWrite(RELAY_LIGHT, LOW);
-        }
+        digitalWrite(RELAY_LIGHT, light ? RELAY_ON : RELAY_OFF);
         xSemaphoreGive(mutex_light);
-#endif
-        vTaskDelay(50 / portTICK_PERIOD_MS);
-        
-        
-        if(previous_temp1 != digitalRead(BUTTON_UP)){
+
+
+        //Buttons ---------------------------------
+        vTaskDelay(50 / portTICK_PERIOD_MS); //50 ms for debounce
+        if (previous_temp1 != digitalRead(BUTTON_UP))
+        {
             previous_temp1 = digitalRead(BUTTON_UP);
 
-            if(previous_temp1==0){
+            if (previous_temp1 == 0)
+            {
                 xSemaphoreTake(mutex_button_heat, portMAX_DELAY);
                 temp_switch++;
                 xSemaphoreGive(mutex_button_heat);
             }
             Serial.println(temp_switch);
         }
-        
-        if(previous_temp2 != digitalRead(BUTTON_DOWN)){
+
+        if (previous_temp2 != digitalRead(BUTTON_DOWN))
+        {
             previous_temp2 = digitalRead(BUTTON_DOWN);
 
-            if(previous_temp2==0){
+            if (previous_temp2 == 0)
+            {
                 xSemaphoreTake(mutex_button_heat, portMAX_DELAY);
                 temp_switch--;
                 xSemaphoreGive(mutex_button_heat);
             }
             Serial.println(temp_switch);
-        }      
+        }
 
-        if(previous_light != digitalRead(BUTTON_LIGHT)){
+        if (previous_light != digitalRead(BUTTON_LIGHT))
+        {
             previous_light = digitalRead(BUTTON_LIGHT);
-            if(previous_light==0){
+            if (previous_light == 0)
+            {
                 xSemaphoreTake(mutex_button_light, portMAX_DELAY);
                 light_switch = 1;
                 xSemaphoreGive(mutex_button_light);
@@ -214,7 +180,6 @@ void apply_gpio(void *parameter)
 
 void init_wifi(void)
 {
-    //WiFiMulti WiFiMulti;
     WiFi.begin("HOMETS", "VerySecurePassword3!");
 
     while (WiFi.status() != WL_CONNECTED)
@@ -246,7 +211,7 @@ void init_wifi(void)
 void wifi_client(void *parameter)
 {
     const uint16_t port = 3333;
-    const char *host = "10.10.10.10"; // ip or dns
+    const char *host = "10.10.10.10"; // ip
 
     for (;;)
     {
@@ -259,12 +224,6 @@ void wifi_client(void *parameter)
             Serial.println("Waiting 5 seconds before retrying...");
             vTaskDelay(500 / portTICK_PERIOD_MS);
         }
-
-        // This will send a request to the server
-        //uncomment this line to send an arbitrary string to the server
-        //client.print("Send this data to the server");
-        //uncomment this line to send a basic document request to the server
-
         int maxloops = 0;
 
         //wait for the server's reply to become available
@@ -328,7 +287,6 @@ uint32_t decode_message(String msg, WiFiClient *local)
     wifi_type_t type_command = UNKNOWN_TYPE;
     wifi_operator_t op_command = UNKNOWN_OPERATOR;
     wifi_system_t system_command = UNKNOWN_SYSTEM;
-    wifi_auth_t auth_command = UNKNOWN_AUTH;
     int32_t i = 0;
     int32_t j = 0;
     int32_t k = 0;
@@ -364,7 +322,6 @@ uint32_t decode_message(String msg, WiFiClient *local)
         switch (type_command)
         {
         case TEMP:
-#if ENABLE_HEAT_SENSOR
             current_index = sub_msg.indexOf(STRING_DELIMITER);
             if (current_index <= 0)
             {
@@ -390,16 +347,14 @@ uint32_t decode_message(String msg, WiFiClient *local)
                 break;
             case GET:
                 xSemaphoreTake(mutex_read_temp, portMAX_DELAY);
-                local->printf("%f\r", old_temp);
+                local->printf("%f\r", old_temp);  // Set the temp value
                 xSemaphoreGive(mutex_read_temp);
                 break;
             default:
                 return 0;
             }
-#endif
             break;
         case LIGHT:
-#if ENABLE_LIGHT_SENSOR
             current_index = sub_msg.indexOf(STRING_DELIMITER);
             if (current_index <= 0)
             {
@@ -428,7 +383,6 @@ uint32_t decode_message(String msg, WiFiClient *local)
             default:
                 return 0;
             }
-#endif
             break;
         default:
             return 0;
@@ -451,7 +405,6 @@ uint32_t decode_message(String msg, WiFiClient *local)
         switch (type_command)
         {
         case TEMP:
-#if ENABLE_HEAT_CONTROL
             current_index = sub_msg.indexOf(STRING_DELIMITER);
             if (current_index <= 0)
             {
@@ -473,21 +426,19 @@ uint32_t decode_message(String msg, WiFiClient *local)
             {
             case SET:
                 xSemaphoreTake(mutex_heat, portMAX_DELAY);
-                heat =(bool) sub_msg.toInt();
+                heat = (bool)sub_msg.toInt(); // Set the temp relay value
                 xSemaphoreGive(mutex_heat);
                 break;
             case GET:
                 xSemaphoreTake(mutex_heat, portMAX_DELAY);
-                local->printf("%d\r", heat);
+                local->printf("%d\r", heat); // Get the temp relay value
                 xSemaphoreGive(mutex_heat);
                 break;
             default:
                 return 0;
             }
-#endif
             break;
         case LIGHT:
-#if ENABLE_LIGHT_CONTROL
             current_index = sub_msg.indexOf(STRING_DELIMITER);
             if (current_index <= 0)
             {
@@ -509,24 +460,23 @@ uint32_t decode_message(String msg, WiFiClient *local)
             {
             case SET:
                 xSemaphoreTake(mutex_light, portMAX_DELAY);
-                light =(bool) sub_msg.toInt();
+                light = (bool)sub_msg.toInt(); // Set the light relay value
                 xSemaphoreGive(mutex_light);
                 break;
             case GET:
                 xSemaphoreTake(mutex_light, portMAX_DELAY);
-                local->printf("%d\r", light);
+                local->printf("%d\r", light); // Get the light relay value
                 xSemaphoreGive(mutex_light);
                 break;
             default:
                 return 0;
             }
             break;
-#endif
         default:
             return 0;
         }
         break;
-        case BUTTON:
+    case BUTTON:
         current_index = sub_msg.indexOf(STRING_DELIMITER);
         token = sub_msg.substring(0, current_index);
         sub_msg = sub_msg.substring(current_index + 1);
@@ -544,7 +494,6 @@ uint32_t decode_message(String msg, WiFiClient *local)
         switch (type_command)
         {
         case TEMP:
-#if ENABLE_HEAT_SENSOR
             current_index = sub_msg.indexOf(STRING_DELIMITER);
             if (current_index <= 0)
             {
@@ -570,17 +519,15 @@ uint32_t decode_message(String msg, WiFiClient *local)
                 break;
             case GET:
                 xSemaphoreTake(mutex_button_heat, portMAX_DELAY);
-                local->printf("%d\r", temp_switch);
+                local->printf("%d\r", temp_switch);  // send the value of the temp buttons
                 temp_switch = 0;
                 xSemaphoreGive(mutex_button_heat);
                 break;
             default:
                 return 0;
             }
-#endif
             break;
         case LIGHT:
-#if ENABLE_LIGHT_SENSOR
             current_index = sub_msg.indexOf(STRING_DELIMITER);
             if (current_index <= 0)
             {
@@ -605,50 +552,18 @@ uint32_t decode_message(String msg, WiFiClient *local)
                 break;
             case GET:
                 xSemaphoreTake(mutex_button_light, portMAX_DELAY);
-                local->printf("%d\r", light_switch);
+                local->printf("%d\r", light_switch); // send the value of the light button
                 light_switch = 0;
                 xSemaphoreGive(mutex_button_light);
                 break;
             default:
                 return 0;
             }
-#endif
             break;
         default:
             return 0;
         }
         break;
-    case REFRESH:
-        current_index = sub_msg.indexOf(STRING_DELIMITER);
-        if (current_index <= 0)
-        {
-            current_index = sub_msg.length();
-        }
-        token = sub_msg.substring(0, current_index);
-        sub_msg = sub_msg.substring(current_index + 1);
-        while (token.compareTo(WIFI_OPERATOR_TYPE_STRING[j]) != 0)
-        {
-            j++;
-            vTaskDelay(1 / portTICK_PERIOD_MS);
-            if (j == WIFI_OPERATOR_LENGHT)
-            {
-                return 0;
-            }
-            op_command = (wifi_operator_t)j;
-        }
-        switch (op_command)
-        {
-        case SET:
-            //todo ajouter le refesh
-            break;
-        case GET:
-            //todo ajouter le refesh
-            break;
-        default:
-            return 0;
-        }
-        break;
-
     case SYSTEM:
         current_index = sub_msg.indexOf(STRING_DELIMITER);
         if (current_index <= 0)
@@ -673,53 +588,19 @@ uint32_t decode_message(String msg, WiFiClient *local)
             //todo add reset
             break;
         case START:
-            local->printf("1");
+            local->printf("1"); //send that the sensor is started
             break;
         case RESTART:
-            //todo add restart
-            ESP.restart();
-
+            ESP.restart(); //restart the sensor
             break;
         case STOP:
             //todo add stop
             break;
-        case WIFI:
-            current_index = sub_msg.indexOf(STRING_DELIMITER);
-            if (current_index <= 0)
-            {
-                current_index = sub_msg.length();
-            }
-            token = sub_msg.substring(0, current_index);
-            sub_msg = sub_msg.substring(current_index + 1);
-            while (token.compareTo(WIFI_AUTH_TYPE_STRING[k]) != 0)
-            {
-                k++;
-                vTaskDelay(1 / portTICK_PERIOD_MS);
-                if (k == WIFI_AUTH_LENGHT)
-                {
-                    return 0;
-                }
-                auth_command = (wifi_auth_t)k;
-            }
-            switch (auth_command)
-            {
-            case PASSWORD:
-                //todo ajouter la modification password
-                break;
-            case SSID:
-                //todo ajouter le modification du nom
-                break;
-            case APPLY:
-                //todo ajouter le apply
-                break;
-            default:
-                return 0;
-            }
-            break;
         case STATUS_DEVICE:
+            //todo add status
             break;
         case GET_ID:
-            local->printf("%d\r", (uint32_t) DEVICE_ID);
+            local->printf("%d\r", (uint32_t)DEVICE_ID);
             break;
         default:
             return 0;
